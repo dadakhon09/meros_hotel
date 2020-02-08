99caea2a026fc4bf25793284e7688f3d32214985
dadakhon09 <odadaxon27@mail.ru>
2020-02-08 22:29:34 +0500

    ...

--- main/contact/views.py
+++ main/contact/views.py
@@ -1,4 +1,7 @@
+from django.core.mail import EmailMessage
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 
 
@@ -6,3 +9,14 @@ class ContactView(View):
     def get(self, request):
         return render(request, 'main/contact.html')
 
+    def post(self, request):
+        full_name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        m = EmailMessage(subject,
+                         f'Name: {full_name}\n Email: {email}\n Message:\n{message}',
+                         'odadaxon99@gmail.com', ['odadaxon99@gmail.com', ])
+        m.send()
+        return HttpResponseRedirect(reverse('contact'))
--- main/gallery/views.py
+++ main/gallery/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from adminka.model import Gallery
 from adminka.model.room import RoomImage
 
 
 class GalleryView(View):
     def get(self, request):
         r_images = RoomImage.objects.all()
+        g_images = Gallery.objects.all()
 
-        return render(request, 'main/gallery.html', {'r_images': r_images})
+        return render(request, 'main/gallery.html', {'r_images': r_images, 'g_images': g_images})
