4c7465cd0452cdfba3c830b9cb77d43df4355e13
dadakhon09 <odadaxon27@mail.ru>
2020-02-15 23:56:13 +0500

    ...

--- main/rooms/views.py
+++ main/rooms/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from adminka.model.room import Room
+from adminka.model.room import Room, RoomImage
 
 
 class RoomsView(View):
@@ -14,5 +14,5 @@ class RoomsView(View):
 class RoomView(View):
     def get(self, request, slug):
         room = Room.objects.get(slug=slug)
-
-        return render(request, 'main/accomodation.html', {'room': room})
+        r_images = RoomImage.objects.filter(room=room)
+        return render(request, 'main/accomodation_view.html', {'room': room, 'r_images': r_images})
